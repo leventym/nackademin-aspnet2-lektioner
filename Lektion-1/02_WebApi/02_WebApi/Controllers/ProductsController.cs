5e458f31120fc07e6313404736bf49504de5a4fc
Levent Yildirim <leventym86@hotmail.com>
2022-04-05 12:05:35 +0200

    web api

---
+++ Lektion-1/02_WebApi/02_WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using _02_WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private List<Product> _products;
+        public IEnumerable<Product> GetAll()
+        {
+            
+            return _products;
+        }
+
+        public void Create(Product model)
+        {
+            _products.Add(model);
+        }
+    }
+}
