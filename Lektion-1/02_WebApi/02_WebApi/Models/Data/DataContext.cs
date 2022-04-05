8d77d6c5ae5a713c5e926ca1d2930226426b953e
Levent Yildirim <leventym86@hotmail.com>
2022-04-05 13:49:16 +0200

    sql

---
+++ Lektion-1/02_WebApi/02_WebApi/Models/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using _02_WebApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02_WebApi.Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<ProductEntity> Products { get; set; } = null!;
+    }
+}
