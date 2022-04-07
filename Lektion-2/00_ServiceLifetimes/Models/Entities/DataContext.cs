4d3336f2d541e500b117dfbdb2121fe06c6dd35c
Levent Yildirim <leventym86@hotmail.com>
2022-04-07 14:35:07 +0200

    new project azure

---
+++ Lektion-2/00_ServiceLifetimes/Models/Entities/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _00_ServiceLifetimes.Models.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
