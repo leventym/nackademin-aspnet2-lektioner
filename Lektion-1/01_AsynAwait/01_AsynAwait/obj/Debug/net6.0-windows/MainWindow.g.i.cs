df89b7ca068baf9af3978f176b737723ce241918
Levent Yildirim <leventym86@hotmail.com>
2022-04-07 14:38:32 +0200

    async

--- Lektion-1/01_AsynAwait/01_AsynAwait/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Lektion-1/01_AsynAwait/01_AsynAwait/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace _01_AsynAwait {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/01_AsynAwait;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/01_AsynAwait;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
