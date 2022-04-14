09ec334d18919b0ff26aa2aa5cb7a3328e1944fb
Levent Yildirim <leventym86@hotmail.com>
2022-04-14 11:22:56 +0200

    testing

---
+++ Lektion-4/WebApi.Test/Systems/Controllers/ProductsController_Should.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using Xunit;
+
+namespace WebApi.Test.Systems.Controllers
+{
+    
+    public class ProductsController_Should
+    {
+        [Fact]
+        public async Task GetAllProducts_OnSuccess_Return_StatusCode_200()
+        {
+            //Arrange
+            var sut = new ProductsController();
+
+            //Act
+            var result = (OkObjectResult)await sut.GetAllProducts();
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
---
+++ Lektion-4/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return new OkObjectResult("");
+        }
+    }
+}
