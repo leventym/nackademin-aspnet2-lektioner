26bfd2a782bd88ec3728697b052f925bcbc343db
Levent <leventym86@hotmail.com>
2022-05-12 15:41:46 +0200

    external accounts

--- Lektion-10/02_Identity_ExternallAccounts/Data/Migrations/20220512132818_init.Designer.cs
+++ Lektion-10/02_Identity_ExternallAccounts/Data/Migrations/20220512132818_init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _02_Identity_ExternallAccounts.Data;
 
@@ -11,9 +12,10 @@ using _02_Identity_ExternallAccounts.Data;
 namespace _02_Identity_ExternallAccounts.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220512132818_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
