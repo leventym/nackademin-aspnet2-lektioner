dae0364d9651091537641eaa57c66addc24c479c
Levent <leventym86@hotmail.com>
2022-05-12 23:09:51 +0200

    added azure ad

---
+++ Lektion-10/03_Identity_AzureAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _03_Identity_AzureAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
