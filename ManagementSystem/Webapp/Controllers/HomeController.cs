5697d44982ad668942aad7d2db43a95c31ff0923
Levent <leventym86@hotmail.com>
2022-05-14 00:19:58 +0200

    new project with test

---
+++ ManagementSystem/Webapp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Webapp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ ManagementSystem/Webapp/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webapp.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
